4a2357b0b27ff165eed9a46b776613ff2cb380fc
Mathieu Savy <mathieu.savy@gmail.com>
2015-04-08 09:34:33 +0200

    Initial commit

---
+++ Swift-NSTableView/MyCellView.swift
@@ -0,0 +1,17 @@
+//
+//  MyCellView.swift
+//  Totoproject
+//
+//  Created by Mathieu Savy on 07/04/2015.
+//  Copyright (c) 2015 Mathieu Savy. All rights reserved.
+//
+
+import Cocoa
+
+class MyCellView: NSView {
+    
+    @IBOutlet weak var itemName: NSTextField!
+    @IBOutlet weak var itemDescription: NSTextField!
+    @IBOutlet weak var itemImage: NSImageView!
+    
+}
--- Swift-NSTableView/ViewController.swift
+++ Swift-NSTableView/ViewController.swift
@@ -8,12 +8,30 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+    @IBOutlet weak var tableView: NSTableView!
+    
+    var data: [[String: String]]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        data = [
+            [
+                "name": "XCode",
+                "description": "XCode is an amazing IDE",
+                "image": "xcode-6.png"
+            ],
+            [
+                "name": "WebStorm",
+                "description": "Webstorm is amazing for web development",
+                "image": "webstorm.png"
+            ]
+        ];
+        
+        let nib = NSNib(nibNamed: "MyCellView", bundle: NSBundle.mainBundle())
+        tableView.registerNib(nib!, forIdentifier: "MyCellView")
     }
 
     override var representedObject: AnyObject? {
@@ -22,6 +40,24 @@ class ViewController: NSViewController {
         }
     }
 
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return data!.count
+    }
+    
+    func tableView(tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = tableView.makeViewWithIdentifier("MyCellView", owner: self) as MyCellView
+        let item: [String: String] = (data!)[row]
+        
+        cell.itemName.stringValue = item["name"]!
+        cell.itemDescription.stringValue = item["description"]!
+        cell.itemImage.image = NSImage(named: item["image"]!)
+        
+        return cell
+    }
 
 }
 
