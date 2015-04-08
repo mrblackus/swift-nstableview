c80a207a2a1656742c0289613da14539e01ebd48
Mathieu Savy <mathieu.savy@gmail.com>
2015-04-08 09:01:57 +0200

    Initial Commit

---
+++ Swift-NSTableView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift-NSTableView
+//
+//  Created by Mathieu Savy on 08/04/2015.
+//  Copyright (c) 2015 Mathieu Savy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift-NSTableView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift-NSTableView
+//
+//  Created by Mathieu Savy on 08/04/2015.
+//  Copyright (c) 2015 Mathieu Savy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
